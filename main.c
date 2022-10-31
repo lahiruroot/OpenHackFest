5090c29891f1b8e092e4a4b2aa22772dcd812135
btennakoon <112370084+btennakoon@users.noreply.github.com>
2022-10-31 22:03:03 +0530

    Main

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    char name[]= "Hacktoberfest";
+    int age= 23;
+
+    if(age>=20){
+        printf("Accepted:  %s", name);
+    }
+
+    else{
+        printf("Not Accepted");
+    }
+}
